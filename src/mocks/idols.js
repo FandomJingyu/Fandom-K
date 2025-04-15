1e32f6901244a2c3e792a226261d2002301be1f9
sumin <tnals2565@naver.com>
2025-04-16 00:35:06 +0900

    ✨ Feat: 깃모지, 커밋린트 설정 추가 및 디렉토리 세팅 (#7)
    깃모지, 커밋린트 설정 추가 및 디렉토리 세팅

---
+++ src/mocks/idols.js
@@ -0,0 +1,46 @@
+export const IDOLS = [
+  {
+    voteId: 1,
+    idol: {
+      totalVotes: 1000,
+      profilePicture: '/mocks/mock01.png',
+      group: '뉴진스',
+      gender: 'female',
+      name: '민지',
+      id: 0,
+    },
+  },
+  {
+    voteId: 2,
+    idol: {
+      totalVotes: 800,
+      profilePicture: '/mocks/mock02.png',
+      group: '아이브',
+      gender: 'female',
+      name: '장원영',
+      id: 1,
+    },
+  },
+  {
+    voteId: 3,
+    idol: {
+      totalVotes: 750,
+      profilePicture: '/mocks/mock03.png',
+      group: '르세라핌',
+      gender: 'female',
+      name: '채원',
+      id: 2,
+    },
+  },
+  {
+    voteId: 4,
+    idol: {
+      totalVotes: 650,
+      profilePicture: '/mocks/mock04.png',
+      group: '블랙핑크',
+      gender: 'female',
+      name: '제니',
+      id: 3,
+    },
+  },
+];
---
+++ src/pages/Landing/index.jsx
@@ -0,0 +1,11 @@
+import TestComponents from '../../components/testComponents';
+
+const Landing = () => {
+  return (
+    <div>
+      <TestComponents />
+    </div>
+  )
+}
+
+export default Landing
