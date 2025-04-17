2cef1cf54b9f00a814263dcb9453dce4ea9dbc2d
sumin <tnals2565@naver.com>
2025-04-17 15:42:49 +0900

    ✨ Feat: 랜딩페이지 PC UI 구현 및 애니메이션 작업 완료
    Resolves: #9
    
    - 랜딩페이지 PC UI, 애니메이션 마이그레이션 작업
    - 폰트누락 cdn연결
    - infinite carousel util함수 추가
    - gsap, lenis 추가했습니다`npm install` 한 번 해주세요!

---
+++ src/pages/Landing/components/LandingHeader.jsx
@@ -0,0 +1,71 @@
+import styled from "@emotion/styled";
+import { useEffect, useRef } from "react";
+
+export default function LandingHeader() {
+	const headerRef = useRef(null);
+	const prevScrollPosRef = useRef(0);
+
+	const handleScroll = useCallback(() => {
+		const currentScrollPos = window.scrollY;
+
+		if (headerRef.current) {
+			if (prevScrollPosRef.current > currentScrollPos) {
+				headerRef.current.style.transform = "translateY(0)";
+			} else {
+				headerRef.current.style.transform = "translateY(-100%)";
+			}
+		}
+
+		prevScrollPosRef.current = currentScrollPos;
+	}, []);
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [handleScroll]);
+
+	return (
+		<StyledLandingHeader ref={headerRef}>
+			<div className="landingGrid">
+				<h1>
+					<img src="/images/landing/logo.svg" alt="Fandom-K" />
+				</h1>
+				<button type="button" className="landingBtn">
+					지금 시작하기
+				</button>
+			</div>
+		</StyledLandingHeader>
+	);
+}
+
+const StyledLandingHeader = styled.div`
+  background-color: rgba(0, 0, 0, 0.8);
+  height: 100px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 100;
+  transition: transform 0.3s ease-in-out;
+  .landingGrid {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100%;
+  }
+  .landingBtn {
+    height: 48px;
+    width: 160px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: linear-gradient(90deg, #f86f65 0%, #fe5493 100%);
+    border-radius: 10px;
+    font-size: 16px;
+    font-weight: 700;
+    color: #fff;
+  }
+`;
---
+++ src/pages/Landing/components/LandingTopSection.jsx
@@ -0,0 +1,197 @@
+import styled from "@emotion/styled";
+import { gsap } from "gsap";
+import { useEffect } from "react";
+
+export default function LandingTopSection() {
+	useEffect(() => {
+		// 랜딩 직후 fade 애니메이션
+		const fadeInCtx = gsap.context(() => {
+			gsap.set(".landingTopSection", {
+				autoAlpha: 0,
+			});
+
+			gsap.to(".landingTopSection", {
+				autoAlpha: 1,
+				duration: 1,
+				delay: 0.3,
+			});
+		});
+		// LandingTopSection 카드 애니메이션
+		const cardAnimCtx = gsap.context(() => {
+			const anim = gsap.timeline({
+				scrollTrigger: {
+					trigger: ".landingTopSection",
+					start: "top top",
+					end: "+=200%",
+					pin: true,
+					scrub: 1,
+				},
+			});
+
+			gsap.set(".landingTopSectionCards li", {
+				autoAlpha: 0,
+				y: 100,
+			});
+
+			anim
+				.to(
+					".landingTopSection h2",
+					{
+						gap: 400,
+						duration: 1,
+					},
+					0,
+				)
+				.to(
+					".landingTopSectionDesc",
+					{
+						autoAlpha: 0,
+						duration: 1,
+					},
+					0,
+				)
+				.to(".landingTopSectionCards .item01", {
+					autoAlpha: 1,
+					y: 0,
+					duration: 1,
+				})
+				.to(".landingTopSectionCards .item02", {
+					autoAlpha: 1,
+					y: 0,
+					duration: 1,
+				})
+				.to(".landingTopSectionCards .item03", {
+					autoAlpha: 1,
+					y: 0,
+					duration: 1,
+				})
+				.to(".landingTopSection", {
+					delay: 0.5,
+				});
+		});
+		return () => {
+			fadeInCtx.revert();
+			cardAnimCtx.revert();
+		};
+	}, []);
+
+	return (
+		<StyledLandingTopSection className="landingTopSection">
+			<div className="blur blur01">
+				<img src="/images/landing/blur01.png" alt="blur" />
+			</div>
+			<div className="blur blur02">
+				<img src="/images/landing/blur02.png" alt="blur" />
+			</div>
+			<div className="landingGrid">
+				<h2>
+					<p>좋아하는 만큼</p>
+					<ul className="landingTopSectionCards">
+						<li className="item01">
+							<img src="/images/landing/landing_fade01.png" alt="" />
+						</li>
+						<li className="item02">
+							<img src="/images/landing/landing_fade02.png" alt="" />
+						</li>
+						<li className="item03">
+							<img src="/images/landing/landing_fade03.png" alt="" />
+						</li>
+					</ul>
+					<p>
+						<span>덕질</span>은 쉽게
+					</p>
+				</h2>
+				<p className="landingTopSectionDesc">
+					아티스트에게 내 마음을 전하고 싶을 때,
+					<br />
+					가장 먼저 떠오르는 방법이 되어드릴게요.
+				</p>
+			</div>
+		</StyledLandingTopSection>
+	);
+}
+
+const StyledLandingTopSection = styled.section`
+  height: 100vh;
+  .blur {
+    position: absolute;
+    width: 800px;
+    height: 800px;
+    z-index: 1;
+    &.blur01 {
+      top: 100px;
+      left: -400px;
+    }
+    &.blur02 {
+      bottom: 100px;
+      right: -400px;
+    }
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  .landingGrid {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    > p {
+      margin-top: 100px;
+      color: #888;
+      text-align: center;
+      font-size: 26px;
+      font-weight: 400;
+      line-height: 1.5;
+      height: 0;
+      overflow: visible;
+    }
+  }
+  h2 {
+    width: 100%;
+    color: #fff;
+    text-align: center;
+    font-size: 100px;
+    font-weight: 700;
+    line-height: 1.5;
+    display: flex;
+    justify-content: center;
+    gap: 24px;
+    p {
+      span {
+        color: #f96d69;
+      }
+    }
+  }
+  .landingTopSectionCards {
+    position: absolute;
+    width: 380px;
+    height: 460px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10;
+    margin-left: 46px;
+    li {
+      position: absolute;
+      width: 300px;
+      height: 380px;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+      &.item02 {
+        margin-top: 40px;
+        margin-left: 40px;
+      }
+      &.item03 {
+        margin-top: 80px;
+        margin-left: 80px;
+      }
+    }
+  }
+`;
