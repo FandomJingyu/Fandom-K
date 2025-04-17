f5065a55ea222de5e2fd9fdbc748fed5ecaa6ed7
김서연 <rainbow2870@naver.com>
2025-04-17 22:37:47 +0900

    ✨ Feat: Donation 컴포넌트 기본 구조 및 서브 컴포넌트 연결
    - Donation 컴포넌트에 Card, ProgressBar 기본 컴포넌트 연결
    - 각 서브 컴포넌트 파일을 별도로 생성하여 export 설정
    - 관련 이미지 파일 업로드 (credit, btn-donation-arrow-left & right,
      donation-card-over)

---
+++ src/pages/List/Donation/index.jsx
@@ -0,0 +1,12 @@
+import { IDOLS } from "../../../mocks/idols";
+import Card from "./components/Card";
+import ProgressBar from "./components/ProgressBar";
+
+function Donation() {
+	return (
+		<>
+			<Card />
+			<ProgressBar />
+		</>
+	);
+}
