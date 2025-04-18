50aaeb6a4e9953b02630024fd96661d6d5e42149
sumin <tnals2565@naver.com>
2025-04-19 00:26:02 +0900

    ✨ Feat: API 통합 구조 설계 및 공통 모듈 분리
    API 통합 구조 설계 및 공통 모듈 분리 Resolve: #38

--- src/pages/Testpage/Testpage.jsx
+++ src/pages/Testpage/Testpage.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React, { useState, useEffect, useCallback } from "react";
+import { idolsAPI } from "../../apis/idols";
 import Button from "../../components/Button/Button";
 import Modal from "../../components/Modal";
 
@@ -12,6 +15,16 @@ const Testpage = () => {
 	const handleOpen = () => {
 		setIsOpen(true);
 	};
+
+	const [totalList, setTotalList] = useState([]);
+	const fetchIdols = useCallback(async () => {
+		const response = await idolsAPI.getIdols(100);
+		setTotalList(response.list);
+	}, []);
+
+	useEffect(() => {
+		fetchIdols();
+	}, [fetchIdols]);
 
 	return (
 		<div>
@@ -30,8 +43,64 @@ const Testpage = () => {
 			<Modal isOpen={isOpen} onClose={handleClose} title="모달 컨텐츠">
 				<p>여기에 모달 내용이 들어갑니다.</p>
 			</Modal>
+
+			<div css={testIdolsCircle}>
+				{totalList.map((item) => (
+					<div key={item.id}>
+						<img src={item.profilePicture} alt={item.name} />
+					</div>
+				))}
+			</div>
+			<div css={testIdolsDonation}>
+				{totalList.map((item) => (
+					<div key={item.id}>
+						<img src={item.profilePicture} alt={item.name} />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
+
+const testIdolsCircle = css`
+  width: 980px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  > div {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    overflow: hidden;
+    border: 2px solid var(--orange-F96D69);
+    padding: 5px;
+    > img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      object-fit: cover;
+      object-position: center top;
+    }
+  }
+`;
+
+const testIdolsDonation = css`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 20px;
+  width: 1920px;
+  margin: 0 auto;
+  > div {
+    height: 400px;
+    overflow: hidden;
+    > img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center top;
+    }
+  }
+`;
 
 export default Testpage;
