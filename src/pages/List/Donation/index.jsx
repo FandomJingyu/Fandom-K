27e7f7973746705c0ec1916691442a4f4d34a13c
김서연 <rainbow2870@naver.com>
2025-04-18 17:28:00 +0900

    🎨 Feat: [list] - 후원을 기다리는 조공 Donation 컴포넌트 UI 완료
    - Card 컴포넌트를 반복 렌더링하는 구조를 추가했습니다.
    
    - 슬라이더 구조 안에 배치했으며, 버튼을 좌우로 배치했습니다.
    
    - 추후 케러셀 라이브러리 기능 과의 연계를 고려중입니다.
    
    - Resolves: #28

--- src/pages/List/Donation/index.jsx
+++ src/pages/List/Donation/index.jsx
@@ -1,12 +1,43 @@
-import { IDOLS } from "../../../mocks/idols";
+/** @jsxImportSource @emotion/react */
+import { DONATIONS } from "../../../mocks/donations";
 import Card from "./components/Card";
-import ProgressBar from "./components/ProgressBar";
+
+import btnLeft from "/images/btn-donation-arrow-left.svg";
+import btnRight from "/images/btn-donation-arrow-right.svg";
+import {
+	donationContent,
+	donationTitle,
+	donationWrapper,
+	pageNationLeft,
+	pageNationRight,
+	slider,
+} from "./Donation.style";
 
 function Donation() {
+	const donations = DONATIONS;
+
 	return (
-		<>
-			<Card />
-			<ProgressBar />
-		</>
+		<section css={donationWrapper}>
+			<h2 css={donationTitle}>후원을 기다리는 조공</h2>
+
+			<div css={slider}>
+				<button type="button" css={pageNationLeft}>
+					<img src={btnLeft} alt="이전" />
+				</button>
+
+				{/* 카드 리스트만 따로 감쌈 */}
+				<div css={donationContent}>
+					{donations.map((donation) => (
+						<Card key={donation.id} donation={donation} />
+					))}
+				</div>
+
+				<button type="button" css={pageNationRight}>
+					<img src={btnRight} alt="이후" />
+				</button>
+			</div>
+		</section>
 	);
 }
+
+export default Donation;
