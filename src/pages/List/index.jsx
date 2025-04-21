24178eb8ac78503206bf124b2156e4dcbe5b307d
김서연 <rainbow2870@naver.com>
2025-04-21 12:51:54 +0900

    ✨ Feat: List 컴포넌트에 Donation 컴포넌트 연결
    - Donation 컴포넌트를 List 페이지에 연결해 UI 확인 예정

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -1,8 +1,11 @@
+import Donation from "./Donation/index";
 import CreditCharge from "./components/creditCharge";
+
 const List = () => {
 	return (
 		<div className="mainGrid">
 			<CreditCharge />
+			<Donation />
 		</div>
 	);
 };
