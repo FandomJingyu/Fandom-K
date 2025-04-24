7dc3d292d77a31cd7d703f01589abb6964849b9b
sumin <tnals2565@naver.com>
2025-04-24 16:39:04 +0900

    💄 Fix: list페이지 상단에 blur 효과 추가
    list페이지 상단에 blur 효과 추가

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -1,15 +1,35 @@
+import { css } from "@emotion/react";
 import CreditCharge from "./Charge/components/CreditCharge";
 import Chart from "./Chart/index";
 import Donation from "./Donation/index";
+/** @jsxImportSource @emotion/react */
 
 const List = () => {
 	return (
-		<div className="mainGrid">
-			<CreditCharge />
-			<Donation />
-			<Chart />
-		</div>
+		<>
+			<div css={BlurStyle}>
+				<img src="/images/landing/blur01.png" alt="blur" />
+			</div>
+			<div className="mainGrid">
+				<CreditCharge />
+				<Donation />
+				<Chart />
+			</div>
+		</>
 	);
 };
 
 export default List;
+const BlurStyle = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 800px;
+  height: 800px;
+  transform: translate(-50%, -50%);
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
