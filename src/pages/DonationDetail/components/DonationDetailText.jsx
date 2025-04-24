3fec1c44567d9bb3d59581d11920eb74bd41468b
sumin <tnals2565@naver.com>
2025-04-24 11:57:55 +0900

    ✨ Feat: 조사를 유연하게 바꿀 수 있는 postPosition.js 유틸함수로 제작
    조사를 유연하게 바꿀 수 있는 postPosition.js 유틸함수로 제작

--- src/pages/DonationDetail/components/DonationDetailText.jsx
+++ src/pages/DonationDetail/components/DonationDetailText.jsx
@@ -1,24 +1,10 @@
 import { css } from "@emotion/react";
+import withPostPosition from "../../../utils/postPosition";
 
 /** @jsxImportSource @emotion/react */
 export default function DonationDetail({ donation, loading }) {
 	const { idol, targetDonation, deadline, subtitle, title } = donation;
 
-	function withPostPosition(word, josa) {
-		if (!word) return "";
-		const lastChar = word[word.length - 1];
-		const code = lastChar.charCodeAt(0);
-		const hasBatchim = (code - 44032) % 28 !== 0;
-
-		const josaMap = {
-			은는: hasBatchim ? "은" : "는",
-			이가: hasBatchim ? "이" : "가",
-			을를: hasBatchim ? "을" : "를",
-			으로: hasBatchim ? "으로" : "로",
-		};
-
-		return word + (josaMap[josa] || "");
-	}
 	const idolWithGa = idol ? withPostPosition(idol.name, "이가") : "";
 	const idolWithEun = idol ? withPostPosition(idol.name, "은는") : "";
 	const donationWithEul = donation
