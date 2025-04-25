57de96efbd33bbfb84d8183df12cfd1b963bcb61
김서연 <rainbow2870@naver.com>
2025-04-25 16:07:47 +0900

    ✨ Refactor: 로딩시 스켈레톤 UI 컴포넌트 연결
    Resolves:# 120

--- src/pages/List/Donation/index.jsx
+++ src/pages/List/Donation/index.jsx
@@ -5,6 +5,7 @@ import LodaingError from "../../../components/Error";
 import { useDonations } from "../../../hooks/useDonation";
 import Card from "./components/Card";
 import Carousel from "./components/Carousel";
+import DonationSkeletion from "./components/Skeleton";
 
 function Donation() {
 	const { donations, loading, error } = useDonations();
@@ -14,7 +15,9 @@ function Donation() {
 			<h2 css={donationTitle}>후원을 기다리는 조공</h2>
 
 			{loading ? (
-				<div>로딩 중...</div>
+				<div>
+					<DonationSkeletion />
+				</div>
 			) : error ? (
 				<LodaingError />
 			) : (
